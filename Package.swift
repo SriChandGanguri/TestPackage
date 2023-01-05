2d966baac2be38855b5157af73184f5e9ec8e6c1
Sri Chand Ganguri <srichand@uvik.in>
2023-01-05 14:44:37 +0530

    Initial Commit

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TestPackage",
-            targets: ["TestPackage"]),
+            targets: ["TestFramework"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +18,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "TestPackage",
-            dependencies: []),
-        .testTarget(
-            name: "TestPackageTests",
-            dependencies: ["TestPackage"]),
+        .binaryTarget(
+            name: "TestFramework",
+            path: "./Sources/TestPackage/TestFramework.xcframework")
     ]
 )
